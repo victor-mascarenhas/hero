0372bd5b79e021f2ba084f22af6d673915b98ece
Victor Mascarenhas <78654482+victor-mascarenhas@users.noreply.github.com>
2024-08-24 12:00:18 -0300

    fix: Nav Btns Mobile

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,20 +3,18 @@ import { useContext } from "react";
 import { CharacterContext } from "../../state/context";
 
 import * as S from "./styles";
-import useResize from "../../hooks/useResize";
 
 const Footer = () => {
   const state = useContext(CharacterContext);
   const { pages, current, numList, onPageChange } = state.pagination;
   const { isLoading } = state.loading;
-  const { isMobile } = useResize();
-  const list = isMobile ? numList.slice(0, 3) : numList;
+
   return (
     <S.FooterBg>
       <Nav
         onPageChange={onPageChange}
         current={current}
-        numList={list}
+        numList={numList}
         pages={pages}
         isLoading={isLoading}
       />
--- src/hooks/usePagination/index.ts
+++ src/hooks/usePagination/index.ts
@@ -1,12 +1,13 @@
 import { useCallback, useState } from "react";
-
-const maxNumBtns = 5;
-const maxLeftBtns = (maxNumBtns - 1) / 2;
-const limit = 10;
+import useResize from "../useResize";
 
 export const usePagination = () => {
   const [offset, setOffset] = useState(0);
   const [total, setTotal] = useState(0);
+  const { isMobile } = useResize();
+  const maxNumBtns = isMobile ? 3 : 5;
+  const maxLeftBtns = (maxNumBtns - 1) / 2;
+  const limit = 10;
   const current = offset ? offset / limit + 1 : 1;
   const pages = Math.ceil(total / limit);
   const maxFirst = Math.max(pages - (maxNumBtns - 1), 1);
